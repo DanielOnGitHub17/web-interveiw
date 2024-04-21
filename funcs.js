539f16c2fed8d575da470efa4fd3b59801c7e6a7
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-04-20 20:41:49 -0500

    styling nice (to me anyway)

--- funcs.js
+++ funcs.js
@@ -6,4 +6,12 @@ let make = (name='div')=>document.createElement(name)
   , getAll = (query)=>[...document.querySelectorAll(query)]
   , identify = ()=>getAll('[id]').forEach(i=>window[i.id] = i)
   , add = (what,to=document.body)=>to.appendChild(what)
-  , bx = (who)=>who.getBoundingClientRect();
+  , bx = (who)=>who.getBoundingClientRect()
+  , show = (what) => what.style.display=""
+  , hide = (what) => what.style.display="none";
+
+
+// switch screen
+function switchScreen(screenID){
+    get(screenID).style.display = ""
+}
