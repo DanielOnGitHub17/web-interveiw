cecc6ccd6fb127be9cb88cd3583f122ceac60371
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-04-27 04:18:20 -0500

    started ai view

--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from .helpers import interview_model
+
+def questions_from_topic(request):
+    details = request.GET
+    if "topic" in details:
+        pass
+    return HttpResponse("Not implemented")
