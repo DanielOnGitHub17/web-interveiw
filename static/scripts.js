acf583bf0757c1abe815c69497b337cbc3baee73
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-05-06 13:10:55 -0500

    formatting and all

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,83 @@
+
+// get user consent for recording screen and making video (remove)
+function consentToRecording(){
+    confirm(`
+    To use this app's video feature, you must share your entire screen
+    <br> and allow the app to record system audio<br>
+    The app does not store recorded data after usage.<br>
+    <b>Your privacy is paramount</b>
+    `, ["decline", "agree"]).then(bool=>{
+        // create a toggle to switch video options
+        createVideoToggle();
+        if (bool){
+            // create voice search ui
+            createVoiceSearchUI();
+        
+            // initialize SpeechSynthesisUtterance
+            TALK = new SpeechSynthesisUtterance();
+            TALK.onend=()=>say.resolve(true);
+        } else{
+            videoSwitch.switch.click();
+            SHOULDVIDEO.remove();
+        }
+    })
+}
+
+
+// get questions. Monitor disableness of button
+function setupAIQuestions() {
+    let rep = AIGEN.reportValidity.bind(AIGEN);
+    AIGEN.reportValidity = ()=>{
+        return rep() &&
+        (textTopic.on?TOPIC:TEXT).value
+    }
+    AIGEN.onsubmit = (event)=>{
+        event.preventDefault();
+        if (AIGEN.reportValidity()){
+            GEN.disabled = true;
+            alert("Questions are being generated in the background. They will appear soon.");
+            let type = (textTopic.on?"topic":"text")
+            , data = new FormData(AIGEN),
+            body = {};
+            body[type] = data.get(type);
+            body["number"] = parseInt(data.get("questno"));
+            // flag
+            DONE = false
+            fetch("/ai/questions/", {
+                method: "post",
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getS(`[name=csrfmiddlewaretoken]`).value
+                },
+            
+            //make sure to serialize your JSON body
+                body: JSON.stringify(body)
+            }).then(response=>{
+                console.log(response)
+                if (response.status != 200){
+                    throw "rejected with error "+response.statusText
+                }
+                response.json().then(obj=>{
+                    console.log(obj);
+                    if (!obj || ["Passed", "Failed"].includes(obj.trim())){
+                        throw "rejected "+obj;
+                        // return;
+                    }
+                    alert("Questions will now be added");
+                    Interview.generateQuestions(JSON.parse(obj)["questions"]);
+                }).catch(error=>{
+                    console.log(error);
+                    alert("Failed to generate questions!");
+                })
+            }).catch(error=>{
+                console.log(error);
+                alert("Failed to generate questions!");
+            });
+        }
+    }
+}
+
 // initialize the loading element, that blocks the screen and all
 function loader(){
     let loading;
@@ -33,7 +113,6 @@ function createTextOrTopic(){
 function createVoiceSearchUI(){
     speechSearch = new SearchUI(VOICETHINGS
         , ()=>speechSynthesis.getVoices().map(i=>i.name)
-        // , ()=>["Daniel", "Ogirimah", "Enesi"]
     ) 
 }
 
@@ -83,81 +162,4 @@ function say(text="", voice){
 // get voice from voice name
 function getVoice(name){
     return speechSynthesis.getVoices().find(i=>i.name == name);
-}
-
-// get user consent for recording screen and making video (remove)
-function consentToRecording(){
-    confirm(`
-    To use this app's video feature, you must share your entire screen
-    <br> and allow the app to record system audio<br>
-    The app does not store recorded data after usage.<br>
-    <b>Your privacy is paramount</b>
-    `, ["decline", "agree"]).then(bool=>{
-        // create a toggle to switch video options
-        createVideoToggle();
-        if (bool){
-            // create voice search ui
-            createVoiceSearchUI();
-        
-            // initialize SpeechSynthesisUtterance
-            TALK = new SpeechSynthesisUtterance();
-            TALK.onend=()=>say.resolve(true);
-        } else{
-            videoSwitch.switch.click();
-            SHOULDVIDEO.remove();
-        }
-    })
-}
-
-// for AI
-
-// get questions. Monitor disableness of button
-function setupAIQuestions() {
-    let rep = AIGEN.reportValidity.bind(AIGEN);
-    AIGEN.reportValidity = ()=>{
-        return rep() &&
-        (textTopic.on?TOPIC:TEXT).value
-    }
-    AIGEN.onsubmit = (event)=>{
-        event.preventDefault();
-        if (AIGEN.reportValidity()){
-            GEN.disabled = true;
-            alert("Questions are being generated in the background. They will appear soon.");
-            let type = (textTopic.on?"topic":"text")
-            , data = new FormData(AIGEN),
-            body = {};
-            body[type] = data.get(type);
-            body["number"] = data.get("questno");
-            // flag
-            DONE = false
-            fetch("/ai/questions/", {
-                method: "post",
-                headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getS(`[name=csrfmiddlewaretoken]`).value
-                },
-            
-            //make sure to serialize your JSON body
-                body: JSON.stringify(body)
-            }).then(response=>{
-                if (response.status != 200){
-                    throw "rejected with error "+response.statusText
-                }
-                response.json().then(obj=>{
-                    if (["Passed", "Failed"].includes(obj)){
-                        throw "rejected "+obj
-                    }
-                    alert("Questions will now be added");
-                    Interview.generateQuestions(JSON.parse(obj)["questions"])
-                }).catch(error=>{
-                    console.log(error);
-                    alert("Failed to generate questions!");
-                })
-            }).catch(error=>{
-                console.log(error);
-                alert("Failed to generate questions!");
-            });
-        }
-    }
 }
