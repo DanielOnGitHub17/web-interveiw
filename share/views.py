aef0532d49bc22d9d05c998a16ff23be7491c1c2
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-05-13 02:21:31 -0500

    main part of share implementation. will add code to check if database is 'full' soon

--- share/views.py
+++ share/views.py
@@ -1,29 +1,36 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpRequest
 from django.shortcuts import render, redirect
 from django.views import View
 import json
 
 from .models import Interview
 from .forms import InterviewForm
-from ai.helpers import handle_error
+from helpers import handle_error, message_home
 
 # Create your views here.
 class Saved(View):
     def get(self, request, at):
         try:
             interview = Interview.objects.get(pk=at)
+            request.SHARED = {
+                "QUESTIONS": json.loads(interview.questions),
+                "TEXT_AFTER": interview.text_after,
+                "TEXT_BEFORE": interview.text_before,
+            }
         except Exception as error:
-            print(error)
-            return Http404
-        return render(request, "index.html", {})
+            handle_error(error)
+            return redirect('/')
+        url = request.build_absolute_uri()
+        return message_home(request, f"{url}")
     
     def post(self, request):
         try:
-            interview = InterviewForm(request)
-            print(interview)
-            return HttpResponse("true")
-            redirect(f"/share/{interview.pk}/")
+            interview = InterviewForm(request.POST)
+            if interview.is_valid():
+                saved = interview.save()
+                return redirect(f"/share/{saved.pk}/")
+            else:
+                raise Exception(f"Not valid{interview}")
         except Exception as error:
             handle_error(error)
-        context = {}
-        return HttpResponse("true")
+        return redirect("/")
