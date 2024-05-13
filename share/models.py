6699ec331f8b342628e7c032acd3caca40e7e997
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-05-13 02:20:22 -0500

    made Interview.questions unique

--- share/models.py
+++ share/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Interview(models.Model):
-    questions = models.TextField()
+    questions = models.TextField(unique=True)
     text_before = models.TextField()
     text_after = models.TextField()
 
